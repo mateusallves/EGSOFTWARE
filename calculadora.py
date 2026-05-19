6991948863fc14f7d6941fd1325e184270122da9
L0nu-z1k <007359@libertas.edu.br>
2026-05-18 21:41:28 -0300

    feat:adicionando comentario

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,14 @@
 class Calculadora:
 
+    # +
     def soma(self, a, b):
         return a + b
-
+    # -
     def subtracao(self, a, b):
         return a - b
-
+    # *
     def multiplicacao(self, a, b):
         return a * b
-
+    # /
     def divisao(self, a, b):
         return a / b
